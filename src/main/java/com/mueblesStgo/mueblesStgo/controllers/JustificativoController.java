19ae22854c27b7471429706a192b70952e1a7b96
Angel Vilches U <angel.vilches@usach.cl>
2022-11-05 22:38:09 -0300

    justificativo recepción POST

--- src/main/java/com/mueblesStgo/mueblesStgo/controllers/JustificativoController.java
+++ src/main/java/com/mueblesStgo/mueblesStgo/controllers/JustificativoController.java
@@ -4,15 +4,13 @@ import com.mueblesStgo.mueblesStgo.entities.JustificativoEntity;
 import com.mueblesStgo.mueblesStgo.repositories.JustificativoRepository;
 import com.mueblesStgo.mueblesStgo.services.JustificativoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/justificativo")
+@CrossOrigin
 public class JustificativoController {
     @Autowired
     JustificativoService justificativoService;
