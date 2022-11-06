b5507aa5d6a9b8046660a7a005bec56560e25066
Angel Vilches U <angel.vilches@usach.cl>
2022-11-06 13:43:59 -0300

    Justificación recepción POST conexión BD

--- src/main/java/com/mueblesStgo/mueblesStgo/controllers/JustificativoController.java
+++ src/main/java/com/mueblesStgo/mueblesStgo/controllers/JustificativoController.java
@@ -1,12 +1,15 @@
 package com.mueblesStgo.mueblesStgo.controllers;
 
 import com.mueblesStgo.mueblesStgo.entities.JustificativoEntity;
-import com.mueblesStgo.mueblesStgo.repositories.JustificativoRepository;
 import com.mueblesStgo.mueblesStgo.services.JustificativoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/justificativo")
@@ -15,6 +18,8 @@ public class JustificativoController {
     @Autowired
     JustificativoService justificativoService;
 
+    private final Logger log = LoggerFactory.getLogger(JustificativoController.class);
+    /*
     @PostMapping("/ingresarJustificativo")
     public String guardarJustificativo(@RequestParam(name="rut") String r, @RequestParam(name="fecha") String f){
         JustificativoEntity justificativo = new JustificativoEntity();
@@ -23,5 +28,15 @@ public class JustificativoController {
         justificativo.setFechaInasistencia(fechaFormato);
         justificativoService.guardarJustificativo(justificativo);
         return "Recibido";
+    }
+
+     */
+    @PostMapping("/ingresarJustificativo")
+     public @ResponseBody JustificativoEntity guardarJustificativo(
+            @RequestBody JustificativoEntity jsonJustificativo){
+        log.info("Request to create justification: {}", jsonJustificativo);
+        System.out.println(jsonJustificativo.getRutEmpleado() + " " + jsonJustificativo.getFechaInasistencia());
+        JustificativoEntity resultado = justificativoService.guardarJustificativo(jsonJustificativo);
+        return jsonJustificativo;
     }
 }
