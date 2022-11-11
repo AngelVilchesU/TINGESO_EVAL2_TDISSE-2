a8822cdb62d2c6a2c14bf8d95ab001e7e4301f56
Angel Vilches U <angel.vilches@usach.cl>
2022-11-10 23:45:27 -0300

    Mapping function and delete unused lines

--- src/main/java/com/mueblesStgo/mueblesStgo/controllers/JustificativoController.java
+++ src/main/java/com/mueblesStgo/mueblesStgo/controllers/JustificativoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/justificativo")
@@ -14,22 +15,14 @@ public class JustificativoController {
     @Autowired
     JustificativoService justificativoService;
 
-    /*
-    @PostMapping("/ingresarJustificativo")
-    public String guardarJustificativo(@RequestParam(name="rut") String r, @RequestParam(name="fecha") String f){
-        JustificativoEntity justificativo = new JustificativoEntity();
-        justificativo.setRutEmpleado(r);
-        LocalDate fechaFormato = justificativoService.fechaFormato(f);
-        justificativo.setFechaInasistencia(fechaFormato);
-        justificativoService.guardarJustificativo(justificativo);
-        return "Recibido";
+    @GetMapping()
+    public List<JustificativoEntity> obtenerJustificativos(){
+        return justificativoService.obtenerJustificativos();
     }
 
-     */
     @PostMapping("/ingresarJustificativo")
      public String guardarJustificativo(
             @RequestBody JustificativoEntity jsonJustificativo) throws IOException {
-        System.out.println(jsonJustificativo.getRutEmpleado() + " " + jsonJustificativo.getFechaInasistencia());
         JustificativoEntity resultado = justificativoService.guardarJustificativo(jsonJustificativo);
         return "Recibido";
     }
